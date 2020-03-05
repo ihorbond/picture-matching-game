a903928027ae5f8fc8498e263988a277d432d439
Ihor Bodnarchuk <Ihor.Bodnarchuk@fsresidential.com>
2020-03-05 11:36:55 -0500

    assign icons to the grid

--- PicMatchingGame/Form1.cs
+++ PicMatchingGame/Form1.cs
@@ -12,9 +12,33 @@ namespace PicMatchingGame
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
+
+        List<string> icons = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",", "k", "k",
+            "b", "b", "v", "v", "w", "w", "z", "z"
+        };
+
         public Form1()
         {
             InitializeComponent();
+            AssignIconsToSquares();
+        }
+
+        private void AssignIconsToSquares()
+        {
+            foreach(Control c in tableLayoutPanel1.Controls)
+            {
+                if (c is Label iconLabel)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    iconLabel.Visible = false;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+            
         }
     }
 }
