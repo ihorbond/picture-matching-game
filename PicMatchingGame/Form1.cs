d75c7da41c3e5e49b6217d54e9dd3eaa6f055c43
Ihor Bodnarchuk <Ihor.Bodnarchuk@fsresidential.com>
2020-03-05 14:50:49 -0500

    basic version of the game is complete

--- PicMatchingGame/Form1.cs
+++ PicMatchingGame/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -20,6 +21,9 @@ namespace PicMatchingGame
             "b", "b", "v", "v", "w", "w", "z", "z"
         };
 
+        Label firstClicked = null;
+        Label secondClicked = null;
+
         public Form1()
         {
             InitializeComponent();
@@ -34,11 +38,68 @@ namespace PicMatchingGame
                 {
                     int randomNumber = random.Next(icons.Count);
                     iconLabel.Text = icons[randomNumber];
-                    iconLabel.Visible = false;
+                    iconLabel.ForeColor = Color.DeepSkyBlue;
                     icons.RemoveAt(randomNumber);
                 }
             }
             
+        }
+
+        private void label_Click(object sender, EventArgs e)
+        {
+            // The timer is only on after two non-matching 
+            // icons have been shown to the player, 
+            // so ignore any clicks if the timer is running
+            if (timer1.Enabled) return;
+
+            if (sender is Label label && label.ForeColor != Color.Black)
+            {
+                if (firstClicked == null)
+                {
+                    firstClicked = label;
+                    firstClicked.ForeColor = Color.Black;
+                    return;
+                }
+
+                secondClicked = label;
+                secondClicked.ForeColor = Color.Black;
+
+                CheckForWinner();
+
+                if (firstClicked.Text == secondClicked.Text)
+                {
+                    firstClicked = null;
+                    secondClicked = null;
+                    return;
+                }
+
+                timer1.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+
+            firstClicked = null;
+            secondClicked = null;
+        }
+
+        private void CheckForWinner()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                if (control is Label iconLabel && iconLabel.ForeColor == iconLabel.BackColor)
+                {
+                    return;
+                }
+            }
+
+            MessageBox.Show("You matched all the icons!", "Congratulations");
+            Close();
         }
     }
 }
